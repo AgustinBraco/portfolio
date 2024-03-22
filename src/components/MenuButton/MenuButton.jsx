d0624279821c1a150cbeafb52878482d2bcf26ab
AgustinBraco <bracoagustin@gmail.com>
2024-03-22 20:49:58 -0300

    [Main] - Version 1.1.0

--- src/components/MenuButton/MenuButton.jsx
+++ src/components/MenuButton/MenuButton.jsx
@@ -1,11 +1,9 @@
-export const MenuButton = props => {
-	return (
-		<button className={`MenuButton ${props.mode}`} onClick={props.onClick}>
-			<img
-				src={`/assets/menu_${props.mode}.png`}
-				alt='menu_icon'
-				className={`MenuIcon ${props.mode}`}
-			/>
-		</button>
-	);
-};
+export const MenuButton = props => (
+	<button className={`MenuButton ${props.mode}`} onClick={props.onClick}>
+		<img
+			src={`/assets/menu_${props.mode}.png`}
+			alt='menu_icon'
+			className={`MenuIcon ${props.mode}`}
+		/>
+	</button>
+);
