4c173ebb40bc3b4b014bc0bade3aa6b2c1807739
AgustinBraco <bracoagustin@gmail.com>
2024-03-22 22:20:21 -0300

    [Main] - Commit to run deploy with

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,33 +1,31 @@
 import { Link } from 'react-router-dom';
 
-export const Footer = () => {
-	return (
-		<footer className='Footer'>
-			<div>
-				<p className='FooterTitle'>Contacto</p>
-				<p className='FooterText'>bracoagustin@gmail.com</p>
-				<Link
-					target='_blank'
-					className='FooterText'
-					to={'https://www.linkedin.com/in/agust%C3%ADn-braco/'}
-				>
-					linkedin/agustin.braco
-				</Link>
-			</div>
+export const Footer = () => (
+	<footer className='Footer'>
+		<div>
+			<p className='FooterTitle'>Contacto</p>
+			<p className='FooterText'>bracoagustin@gmail.com</p>
+			<Link
+				target='_blank'
+				className='FooterText'
+				to={'https://www.linkedin.com/in/agust%C3%ADn-braco/'}
+			>
+				linkedin/agustin.braco
+			</Link>
+		</div>
 
-			<div>
-				<Link className='FooterLink' to={'/'}>
-					Inicio
-				</Link>
-				<Link className='FooterLink' to={'/projects'}>
-					Proyectos
-				</Link>
-			</div>
+		<div>
+			<Link className='FooterLink' to={'/'}>
+				Inicio
+			</Link>
+			<Link className='FooterLink' to={'/projects'}>
+				Proyectos
+			</Link>
+		</div>
 
-			<div>
-				<hr />
-				<p>Copyright © 2024 all rights reserved.</p>
-			</div>
-		</footer>
-	);
-};
+		<div>
+			<hr />
+			<p>Copyright © 2024 all rights reserved.</p>
+		</div>
+	</footer>
+);
