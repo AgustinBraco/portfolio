4f279fc88cf3dbf127c4680ef6881ea85dd9c430
AgustinBraco <bracoagustin@gmail.com>
2024-03-22 22:22:15 -0300

    [Dev] - Merged main changes

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,7 @@
 import { Link } from 'react-router-dom';
 
-export const Logo = props => {
-	return (
-		<Link to={'/'}>
-			<div className={`LogoImage ${props.mode}`}></div>
-		</Link>
-	);
-};
+export const Logo = props => (
+	<Link to={'/'}>
+		<div className={`LogoImage ${props.mode}`}></div>
+	</Link>
+);
